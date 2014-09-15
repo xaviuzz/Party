f1a33882b4639fbb49498a9b73f30949d2e3fd8a
xaviuzz <xaviuzz@gmail.com>
2014-09-15 19:46:35 +0200

    Party it's an abstraction

---
+++ party.rb
@@ -0,0 +1,3 @@
+module Party
+
+end
---
+++ spec/party_spec.rb
@@ -0,0 +1,14 @@
+require './party'
+require './person'
+
+describe "A Party" do
+
+  before(:each) do
+
+  end
+
+  it "It's an abstraction" do
+    expect{Party.new}.to raise_error
+  end
+
+end
